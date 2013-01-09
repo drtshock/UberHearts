71d802b3ed8d344b3e5fb65e0c3fc635c3fed082
Trent Hensler <thensler@wisc.edu>
2013-01-09 02:29:42 -0600

    Wonder how this works.

--- src/me/shock/uberhearts/DeathListener.java
+++ src/me/shock/uberhearts/DeathListener.java
@@ -6,6 +6,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class DeathListener implements Listener 
@@ -26,22 +27,33 @@ public class DeathListener implements Listener
 	{
 		Player player = event.getPlayer();
 		long maxLives = plugin.getConfig().getLong("max-lives");
-		
+		long currentLives = lives.get(player.getName());
+
 		/**
 		 * Handles when a player has already respawned once.
 		 * Handling it in reverse order I guess.
 		 */
 		if(lives.containsKey(player))
 		{
-			long formerLives = lives.get(player.getName());
-			player.setMaxHealth((int) formerLives - 1);
+			player.setMaxHealth((int) currentLives - 1);
 		}
 		else
 		{
-			lives.put(player.getName(), maxLives -1);
+			lives.put(player.getName(), maxLives - 1);
 		}
 		
 		player.sendMessage(uber + "You have " + ChatColor.RED + lives.get(player.getName()) + ChatColor.WHITE + " lives left.");
 
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event)
+	{
+		long maxLives = plugin.getConfig().getLong("max-lives");
+
+		Player player = event.getPlayer();
+		lives.put(player.getName(), maxLives);
+		long currentLives = lives.get(player.getName());
+		player.sendMessage(uber + "You have " + currentLives + " left.");
 	}
 }
