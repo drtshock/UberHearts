9439ce176f1a75249078b9244bdb132cbaefe7ed
Trent Hensler <thensler@wisc.edu>
2013-01-09 02:23:20 -0600

    See how this works

--- src/me/shock/uberhearts/DeathListener.java
+++ src/me/shock/uberhearts/DeathListener.java
@@ -2,6 +2,7 @@ package me.shock.uberhearts;
 
 import java.util.HashMap;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +12,7 @@ public class DeathListener implements Listener
 {
 
 	public HashMap<String, Long> lives = new HashMap<String, Long>();
+	String uber = ChatColor.RED + "[" + ChatColor.WHITE + "UberHearts" + ChatColor.RED + "]" + ChatColor.WHITE + ": ";
 	
     public Main plugin;
 	
@@ -23,6 +25,23 @@ public class DeathListener implements Listener
 	public void onRespawn(PlayerRespawnEvent event)
 	{
 		Player player = event.getPlayer();
+		long maxLives = plugin.getConfig().getLong("max-lives");
 		
+		/**
+		 * Handles when a player has already respawned once.
+		 * Handling it in reverse order I guess.
+		 */
+		if(lives.containsKey(player))
+		{
+			long formerLives = lives.get(player.getName());
+			player.setMaxHealth((int) formerLives - 1);
+		}
+		else
+		{
+			lives.put(player.getName(), maxLives -1);
+		}
+		
+		player.sendMessage(uber + "You have " + ChatColor.RED + lives.get(player.getName()) + ChatColor.WHITE + " lives left.");
+
 	}
 }
